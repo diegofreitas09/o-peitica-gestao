74a8df368503da289c32d636ac7a678d5076fe1b
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-23 22:51:04 -0300

    Conecta Gestão independente à mesma base central

---
+++ config.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+  BUSINESS_NAME: 'Atacadão da Carne O Peitica',
+  WHATSAPP: '5585989626829',
+  WHATSAPP_BUSINESS_URL: 'https://wa.me/message/NW4MC6V5OVEIK1',
+  ADDRESS: 'Rua Edison Martins, 530, Fortaleza - CE',
+  MAPS_URL: 'https://www.google.com/maps/search/?api=1&query=Rua%20Edison%20Martins%2C%20530%2C%20Fortaleza%20-%20CE',
+  INSTAGRAM_URL: 'https://www.instagram.com/atacadaodacarneopeitica?igsi=cDUyODF0ZTl5OGdv',
+  SHEET_CSV_URL: '',
+  ORDER_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbzD7yCVWL49ZNla13uAzNboyXQMSXHx01PLP0spp6niv_usPK-Fkx_lZjnlvvT4e1muVg/exec',
+  DELIVERY_FEE: 0,
+  DATA_TIMEOUT_MS: 7000
+};
